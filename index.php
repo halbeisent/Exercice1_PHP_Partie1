fb7c6d28125f4db2eb64b04484375800907ca823
halbeisent <dev@halbeisen.fr>
2018-05-16 12:02:48 +0200

    Exercice 1 Partie 1 PHP Terminé et corrigé

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>Exercice 1 Partie 1 PHP</title>
+  </head>
+  <body>
+  <?php
+  $firstPHPVar = 'Ceci est ma première variable en PHP';
+  echo $firstPHPVar;
+  ?>
+  </body>
+</html>
